e5901b2b29a1ca32e5cec811a83b40841438a77a
Assela Pathirana <assela@pathirana.net>
2022-11-04 15:02:29 +0100

    graph added with <script setup>

---
+++ src/components/Graph1.vue
@@ -0,0 +1,34 @@
+<template>
+  <Chart
+    :size="{ width: 1000, height: 800 }"
+    :data="data"
+    :margin="margin"
+    :direction="direction">
+    <template #layers>
+      <Grid strokeDasharray="2,2" />
+      <Line :dataKeys="['name', 'pl']" />
+    </template>
+
+  </Chart>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Chart, Grid, Line } from 'vue3-charts'
+import { plByMonth } from '@/data'
+const data = ref(plByMonth)
+const direction = ref('horizontal')
+const margin = ref({
+      left: 0,
+      top: 20,
+      right: 20,
+      bottom: 0
+})
+
+</script>
+
+<style>
+#app {
+  color: #2ecc71
+}
+</style>
